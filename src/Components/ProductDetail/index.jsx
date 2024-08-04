1c54cba00c3989ad99177c353675403f2dabee51
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-08-04 14:49:58 -0500

    Solved problem with add-to-cart button in the detail product section

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import './styles.css';
-import { XMarkIcon, ShoppingCartIcon} from '@heroicons/react/24/solid';
+import { XMarkIcon, ShoppingCartIcon,CheckCircleIcon} from '@heroicons/react/24/solid';
 import { ShoppingCartContext } from '../../Context';
 
 const ProductDetail = () =>{
@@ -8,7 +8,28 @@ const ProductDetail = () =>{
     const context = useContext(ShoppingCartContext);
     const classMainContainer = `${context.isProductDetailOpen ? 'flex':'hidden'} product-detail flex flex-col items-center fixed right-0 bottom-0 border border-black rounded-lg bg-white `;
 
-    console.log('PRODUCT TO SHOW: ', context.productToShow);
+    const buyButton = (id) =>{
+        const isProductOnCart = context.cartProducts.filter(product => product.id === id).length > 0;
+        if(isProductOnCart){
+            return (
+                <div className='flex w-1/2 justify-evenly text-white'>
+                    <span className=' font-normal text-black'>Already in the cart</span>
+                    <CheckCircleIcon className='h-6 w-6 text-black'/>
+                </div>
+            );
+        }else{
+            return (
+                <div className='flex justify-evenly items-center w-1/2 h-10 rounded-lg bg-black'>
+                    <button onClick = {(event)=>{
+                        event.preventDefault();
+                        event.stopPropagation();
+                        context.addNewProductToCart({...context.productToShow, quantity: 1});
+                    }} className='text-white'>Add to the cart</button>
+                    <ShoppingCartIcon className='h-6 w-6 text-white'/>
+            </div>
+            );
+        }
+    }
 
     return (
         <aside className={classMainContainer}>
@@ -27,10 +48,7 @@ const ProductDetail = () =>{
                 <span className='font-light text-sm max-h-28 overflow-clip'>{context.productToShow.description}</span>
             </p>
 
-            <div className='flex justify-evenly items-center w-1/2 h-10 rounded-lg bg-black'>
-                <button className='text-white'>Add to the cart</button>
-                <ShoppingCartIcon className='h-6 w-6 text-white'/>
-            </div>
+            {buyButton(context.productToShow.id)}
 
         </aside>
     );
