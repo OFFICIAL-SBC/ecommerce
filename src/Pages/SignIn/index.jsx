36d3d456c786b43788e58b25f9047f32e7dfa9e0
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-08-17 08:24:03 -0500

    Im gonna work in the route protection

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,6 +1,6 @@
 import Layout from "../../Components/Layout";
-import { Link } from "react-router-dom";
-import { useContext, useState } from "react";
+import { Link, Navigate } from "react-router-dom";
+import { useContext, useState, useRef } from "react";
 import { ShoppingCartContext } from '../../Context';
 
 function SignIn() {
@@ -15,6 +15,32 @@ function SignIn() {
     const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0: true;
     const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0: true;
     const hasUserAnAccount = !noAccountInLocalState || !noAccountInLocalStorage;
+
+
+    const handleSignIn = ()=>{
+        const stringifiedSignOut = JSON.stringify(false);
+        localStorage.setItem('sign-out',stringifiedSignOut);
+        context.setSignOut(false);
+
+        return <Navigate replace to={'/'}/>
+    }
+
+    const form = useRef(null);
+
+    const createAccount = () => {
+        const formData = new FormData(form.current);
+        const data = {
+            name: formData.get('name'),
+            email: formData.get('email'),
+            password: formData.get('password')
+        }
+
+        console.log(data);
+        const stringifiedAccount = JSON.stringify(data);
+        localStorage.setItem('account',stringifiedAccount);
+        context.setAccount(data);
+        handleSignIn();
+    };
 
     const renderLogin = () => {
         return(
@@ -42,7 +68,6 @@ function SignIn() {
                     disabled={hasUserAnAccount}
                     onClick={()=>setView('create-user-info')}
                     >
-
                         Sign Up
 
                 </button>
@@ -51,7 +76,47 @@ function SignIn() {
     };
 
     const renderCreateUserInfo = () => {
-        
+        return(
+            <form ref={form} action="" className="flex flex-col gap-4 w-80">
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="name" className="font-light text-sm">Your name:</label>
+                    <input
+                        type="text"
+                        id="name"
+                        name="name"
+                        defaultValue={parsedAccount?.name}
+                        placeholder="User's name"
+                        className="rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4"/>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="email" className="font-light text-sm">Your email:</label>
+                    <input
+                        type="text"
+                        id="email"
+                        name="email"
+                        defaultValue={parsedAccount?.email}
+                        placeholder="Email" 
+                        className="rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4"/>
+                </div>
+                <div className="flex flex-col gap-1">
+                    <label htmlFor="password" className="font-light text-sm">Your password: </label>
+                    <input
+                        type="text"
+                        id="password"
+                        name="password"
+                        defaultValue={parsedAccount?.password}
+                        placeholder="********"
+                        className="rounded-lg border border-black placeholder:font-light placeholder:text-sm placeholder:text-black/60 focus:outline-none py-2 px-4"/>
+                </div>
+                <Link to="/">
+                    <button className="bg-black text-white w-full rounded-lg py-3"
+                    onClick={()=> createAccount()}>
+                        Create account
+                    </button>
+                </Link>
+
+            </form>
+        );
     };
 
     const renderView = () => view === 'create-user-info' ? renderCreateUserInfo():renderLogin();
