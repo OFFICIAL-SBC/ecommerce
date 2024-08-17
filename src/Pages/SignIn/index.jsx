bb2c4131a3ae8a4691fd88278fb17af931d01190
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-08-16 22:18:58 -0500

    Working in the sing-up page

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,22 +1,36 @@
-import Layout from "../../Components/Layout"
-import { Link } from "react-router-dom"
+import Layout from "../../Components/Layout";
+import { Link } from "react-router-dom";
+import { useContext, useState } from "react";
+import { ShoppingCartContext } from '../../Context';
 
 function SignIn() {
+    const context = useContext(ShoppingCartContext);
+    const [ view, setView ] = useState('user-info');
 
-    return (
-        <Layout>
-            <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+    //Account 
+    const account = localStorage.getItem('account');
+    const parsedAccount = JSON.parse(account);
+
+    //Has an account?
+    const noAccountInLocalStorage = parsedAccount ? Object.keys(parsedAccount).length === 0: true;
+    const noAccountInLocalState = context.account ? Object.keys(context.account).length === 0: true;
+    const hasUserAnAccount = !noAccountInLocalState || !noAccountInLocalStorage;
+
+    const renderLogin = () => {
+        return(
             <div className="flex flex-col w-80">
                 <p>
                     <span className="font-light text-sm">Email: </span>
-                    <span>sebastian@gamil.com</span>
+                    <span>{parsedAccount?.email}</span>
                 </p>
                 <p>
                     <span className="font-light text-sm">Password: </span>
-                    <span>*******</span>
+                    <span>{parsedAccount?.password}</span>
                 </p>
                 <Link to="/">
-                    <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2">
+                    <button className="bg-black disabled:bg-black/40 text-white w-full rounded-lg py-3 mt-4 mb-2"
+                            disabled={!hasUserAnAccount}
+                    >
                         Log in
                     </button>
                 </Link>
@@ -24,12 +38,28 @@ function SignIn() {
                     <a className="font-light text-xs underline underline-offset-4" href="/">Forgot my password</a>
                 </div>
                 <button
-                    className="border border-black disabled:bg-black/40 disabled:border-black/40 rounded-lg mt-6 py-3">
+                    className="border border-black disabled:bg-black/40 disabled:border-black/40 rounded-lg mt-6 py-3"
+                    disabled={hasUserAnAccount}
+                    onClick={()=>setView('create-user-info')}
+                    >
 
                         Sign Up
 
                 </button>
-            </div>
+        </div>
+        );
+    };
+
+    const renderCreateUserInfo = () => {
+        
+    };
+
+    const renderView = () => view === 'create-user-info' ? renderCreateUserInfo():renderLogin();
+
+    return (
+        <Layout>
+            <h1 className="font-medium text-xl text-center mb-6 w-80">Welcome</h1>
+            {renderView()}
         </Layout>
     )
 }
