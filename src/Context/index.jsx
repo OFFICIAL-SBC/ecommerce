9cd74f1ff4da30fe816a5e6e98a15ad41eeaf480
OFFICIAL-SBC <sebastian.bonillac@udea.edu.co>
2024-08-23 00:07:34 -0500

    I finish the my account page

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -8,21 +8,12 @@ export const initializeLocalStorage = () => {
     const accountInLocalStorage = localStorage.getItem('account');
     const signOutInLocalStorage = localStorage.getItem('sign-out');
 
-    let parsedAccount;
-    let parsedSignOut;
-
     if(!accountInLocalStorage){
         localStorage.setItem('account',JSON.stringify({}));
-        parsedAccount = {};
-    }else{
-        parsedAccount = JSON.parse(accountInLocalStorage);
     }
 
     if(!signOutInLocalStorage){
-        localStorage.setItem('sign-out',JSON.stringify(false));
-        parsedSignOut=false;
-    }else{
-        parsedSignOut = JSON.parse(signOutInLocalStorage);
+        localStorage.setItem('sign-out',JSON.stringify(true));
     }
 }
 
